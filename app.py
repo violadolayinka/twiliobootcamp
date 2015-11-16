26dfe98163ad757f9d3d62292e11f42616cb6378
violadolayinka <volayinka@gmail.com>
2015-11-16 13:33:02 -0800

    second commit

--- app.py
+++ app.py
@@ -2,4 +2,11 @@ from bootcamp import StudentApp
 
 app = StudentApp("Viola")
 
+@app.route('/business_phone')
+def business_phone():
+    return """<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Dial>+12672502802</Dial>
+</Response>"""
+
 app.run()
