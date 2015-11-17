d93177326fd7e84b45493685ed8a9b20c830a9de
violadolayinka <volayinka@gmail.com>
2015-11-17 13:26:01 -0800

    imported requests*

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from bootcamp import StudentApp
+import requests
+import json
+
+ACCOUNT_SID = "AC08104111c524d56d95117df1d60f2283"
+ACCOUNT_TOKEN = "d5429aa8bd9592b9fd94c0a3b8a9877c"
 
 app = StudentApp("Viola")
 
@@ -12,7 +17,7 @@ def business_phone():
     else:
         return """<?xml version="1.0" encoding="UTF-8"?>
         <Response>
-          <Say>Alice is unavailable</Say>
+          <Say>Viola is unavailable</Say>
           <Record action = "/hangup" />
         </Response>"""
 
