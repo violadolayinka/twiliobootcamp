4b4e4bd622c9df30730b509c061343f611d7f840
violadolayinka <volayinka@gmail.com>
2015-11-17 12:57:18 -0800

    MAde a few more edits

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ app = StudentApp("Viola")
 
 @app.route('/business_phone')
 def business_phone():
-    return """<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-  <Dial>+12672502802</Dial>
-</Response>"""
+    if app.available:
+        return """<?xml version="1.0" encoding="UTF-8"?>
+        <Response>
+          <Dial>+12672502802</Dial>
+        </Response>"""
+    else:
+        return """<?xml version="1.0" encoding="UTF-8"?>
+        <Response>
+          <Say>Alice is unavailable</Say>
+          <Record action = "/hangup" />
+        </Response>"""
 
 app.run()
