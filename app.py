16c17343a392461b9728370d695343c2fdec0e6b
Viola D. Olayinka <violadolayinka@gmail.com>
2019-03-11 13:33:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from bootcamp import StudentApp
 import requests
 import json
 
-ACCOUNT_SID = "AC08104111c524d56d95117df1d60f2283"
-ACCOUNT_TOKEN = "d5429aa8bd9592b9fd94c0a3b8a9877c"
 
 app = StudentApp("Viola")
 
@@ -16,7 +14,7 @@ cities = {
 @app.route('/weather')
 def weather():
     city = request.values.get('city', 'San Francisco')
-    url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=2de143494c0b295cca9337e1e96b00e0"
+    url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&"
     response = requests.get(url)
     json = json.loads(response.content)
     tempK = json.get("main").get("temp")
